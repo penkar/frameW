94370874ec2c277f3dfb8d6d5e806389e9d77fc2
Jeffrey Penkar <jspenkar@gmail.com>
2017-11-03 20:53:47 -0500

    Creator slimmed down alot.

--- src/source/creator.js
+++ src/source/creator.js
@@ -1,51 +1,20 @@
 import * as utilities from './utilities.js'
 
-export const ce = (ele, {style = null, children = null, sClass = '', text = '', attr = '', href = '', id = ''} = {}) => {
-  let html = document.createElement(ele);
-  if(style) setStyle(html, style);
-  if(children) appender(html, children);
-  if(sClass) classset(html, sClass);
-  if(text) innertext(html, text);
-  if(href) innerhref(html, href);
-  if(attr) innerattr(html, attr);
-  if(id) idset(html, id);
-  return html;
-}
-
-export const idset = (ele, id) => {
-  return ele.id = id;
-}
-
-export const innerhref = (ele, text) => {
-  return ele.href = text;
-}
-
-export const innerattr = (ele, text) => {
-  return ele.innerText = text;
-}
-
-export const setStyle = (element, styleObject) => {
-  for(var i in styleObject) {
-    element.style[i] = styleObject[i];
+export const ce = (ele, {style, children = [], sClass, text, href, id, html} = {}) => {
+  let element = document.createElement(ele);
+  for(var i in style) {
+    element.style[i] = style[i];
   }
-  return element;
-}
-
-export const appender = (element, children) => {
   for(var child of children) { element.appendChild(child); }
+  if(sClass) element.className = sClass;
+  if(text) element.innerText = text;
+  if(href) element.href = href;
+  if(id) element.id = id;
+  if(html) element.innerHTML = html;
   return element;
 }
 
-export const fin = (ele) => {
-  // utilities.zeroMargin();
+export function fin(ele) {
   let doc = document.getElementsByTagName('body')[0];
   doc.appendChild(ele);
-}
-
-export const classset = (ele, text) => {
-  return ele.className = text;
-}
-
-export const innertext = (ele, text) => {
-  return ele.innerText = text;
 }
