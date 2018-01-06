0b1ed49b47da98c3423874e41b146adf1654d9d1
JSP <jspenkar@gmail.com>
2018-01-06 13:41:56 -0600

    Style adjustments.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import {ce, fin, hash} from './source';
 import {Body, HeaderRow, TableOfContents} from './execution';
 import {Sample} from './static';
 
-require('./styles/styles.scss');
 require('./styles/app.scss');
 require('./styles/header.scss');
-require('./styles/toc.scss');
+require('./styles/hpage.scss');
+require('./styles/styles.scss');
 require('./styles/slogan.scss');
+require('./styles/toc.scss');
 
 const runner = (hook) => {
   let html = ce({
